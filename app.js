a851765beab1b8c845f6aa9702ad410728debfde
slym758 <slym758@gmail.com>
2023-03-29 13:21:15 +0300

    workspace created

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  res.send(blog);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
