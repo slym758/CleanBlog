c4eb973200452cfc36f8da8512fa6ec5e67844a1
slym758 <slym758@gmail.com>
2023-03-31 16:36:14 +0300

    workspace

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+const Post = require("./models/Post");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pcleant-test-db");
 
 app.set("view engine", "ejs");
 
 //MIDDLEWARS
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -17,6 +28,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add_post", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 const port = 5000;
